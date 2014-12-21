54f3559faed53dd47d16c1a55e2f6797c9f9f812
Antonio J Rossi <antoniojrossi@gmail.com>
2014-12-21 18:25:18 +0100

    Initial Commit

---
+++ PopApp/PopApp.h
@@ -0,0 +1,19 @@
+//
+//  PopApp.h
+//  PopApp
+//
+//  Created by Antonio J Rossi on 21/12/14.
+//  Copyright (c) 2014 Antonio J Rossi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PopApp.
+FOUNDATION_EXPORT double PopAppVersionNumber;
+
+//! Project version string for PopApp.
+FOUNDATION_EXPORT const unsigned char PopAppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PopApp/PublicHeader.h>
+
+
